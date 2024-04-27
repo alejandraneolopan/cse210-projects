0198ad846eda485b2aa3c74bb9a7111755eb30e8
Alejandra Neolopan <alejandra.neolopan@gmail.com>
2024-04-26 23:41:37 -0400

    Preparation 1 and 2 done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,53 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int grade;
+        char gradeText;
+        int additionalPoints;
+        string sign;
+        sign = "";
+        Console.Write("Enter your grade percentage (0-100): ");
+        grade = int.Parse(Console.ReadLine());
+        
+        if (grade >= 90)
+        {
+            gradeText ='A';
+        }
+        else if (grade >= 80)
+        {
+            gradeText ='B';
+        }
+        else if (grade >= 70)
+        {
+            gradeText ='C';
+        }
+        else if (grade >= 60)
+        {
+            gradeText ='D';
+        }
+        else 
+        {
+            gradeText ='F';
+        }
+        additionalPoints = grade % 10;
+        if (additionalPoints >= 7 && additionalPoints<10 && gradeText != 'A' && gradeText != 'F')
+        {
+            sign = "+";
+        }
+        else if (additionalPoints < 3 && additionalPoints>0 && gradeText != 'F')
+        {
+            sign = "-";
+        }
+        Console.WriteLine($"You got {gradeText}{sign}");  
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! you approved the course.");
+        }
+        else
+        {
+            Console.WriteLine("You will got it next time.");
+        }
+
     }
 }
