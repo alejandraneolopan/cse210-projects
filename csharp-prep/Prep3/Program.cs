100b6b72864ba448fa9b156f7802943e0a620665
Alejandra Neolopan <alejandra.neolopan@gmail.com>
2024-04-29 23:27:20 -0400

    Preparation 3 - Guess My Number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.InteropServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Guess My Number game the computer picks a magic number, and then the user tries to guess it. 
+        int magicNumber=0;
+        int userGuess;
+        byte times = 0;
+        Random randomNumber = new Random();
+        magicNumber = randomNumber.Next(1, 100);
+
+        do{
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+            if (magicNumber == userGuess)
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            else if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else 
+            {
+                Console.WriteLine("Lower");
+            }
+
+            times ++;
+
+        }while(magicNumber != userGuess);
+        
+        Console.WriteLine($"Number of attempts: {times}");
+      
     }
 }
