d683d1f491e7071cc56f8b392abbba2889837a72
Alejandra Neolopan <alejandra.neolopan@gmail.com>
2024-05-09 23:52:47 -0400

    Abstraction - Resume and Jobs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company="Microsoft";
+        job1._startYear= 2016;
+        job1._endYear=2020;
+        job1.Display();
+        Job job2 = new Job();
+        job2._jobTitle = "Senior developer";
+        job2._company="Apple";
+        job2._startYear= 2020;
+        job2._endYear= 2024;
+        job2.Display();
+
+        Resume resume = new Resume();
+        resume._name = "Allison Rose";
+        resume.jobs.Add(job1);
+        resume.jobs.Add(job2);
+        resume.Display();
     }
 }
